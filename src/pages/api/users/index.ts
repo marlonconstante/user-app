e36f5d135c543900c951a1fab6b26d4665dfebe9
Marlon Constante <marlon.constante@gmail.com>
2022-05-10 08:34:38 -0300

    feat(cache): memorized users for an hour

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,10 +1,24 @@
+import cache from 'memory-cache';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import type { User, UserResponse } from 'types';
 
+const MILLIS_IN_HOUR = 1000 * 60 * 60;
+
 function fetchUsers(): Promise<User[]> {
-  return fetch(`${process.env.EXTERNAL_API_URL}/users`)
+  const url = `${process.env.EXTERNAL_API_URL}/users`;
+
+  const cachedUsers = cache.get(url);
+  if (cachedUsers) {
+    return cachedUsers;
+  }
+
+  return fetch(url)
     .then((response) => response.json())
-    .then(({ data }: UserResponse) => data);
+    .then(({ data }: UserResponse) => {
+      cache.put(url, data, MILLIS_IN_HOUR);
+
+      return data;
+    });
 }
 
 export default async function handler(
